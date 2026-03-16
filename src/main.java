0723f49b7f57358410dba7ab6762b8cfa3477f91
Koushal1234 <koushedupulapati@gmail.com>
2026-03-16 15:01:32 +0530

    UC2

--- src/main.java
+++ src/main.java
@@ -1,11 +1,78 @@
-public class BookMyStayApp {
+abstract class Room {
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(int beds, int size, double price) {
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+public class main {
+
     public static void main(String[] args) {
-        System.out.println("=================================");
-        System.out.println("        BOOK MY STAY APP         ");
-        System.out.println("        Hotel Booking System     ");
-        System.out.println("        Version 1.0              ");
-        System.out.println("=================================");
-        System.out.println("Welcome to Book My Stay!");
-        System.out.println("Application started successfully.");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Hotel Room Initialization");
+        System.out.println();
+
+        System.out.println("Single Room:");
+        System.out.println("Beds: " + single.getBeds());
+        System.out.println("Size: " + single.getSize() + " sqft");
+        System.out.println("Price per night: " + single.getPrice());
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        System.out.println("Double Room:");
+        System.out.println("Beds: " + doubleRoom.getBeds());
+        System.out.println("Size: " + doubleRoom.getSize() + " sqft");
+        System.out.println("Price per night: " + doubleRoom.getPrice());
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        System.out.println("Beds: " + suite.getBeds());
+        System.out.println("Size: " + suite.getSize() + " sqft");
+        System.out.println("Price per night: " + suite.getPrice());
+        System.out.println("Available: " + suiteAvailable);
     }
 }
